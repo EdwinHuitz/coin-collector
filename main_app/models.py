5b546e43d4e7d02b0f5512e54998a79b4a234f19
Edwin Huitz <EdwinHuitz@users.noreply.github.com>
2020-10-07 15:33:45 -0400

    adding new coin grade values to model

--- main_app/models.py
+++ main_app/models.py
@@ -2,6 +2,39 @@ from django.db import models
 from django.urls import reverse
 # Create your models here.
 #name, origin, year, denom, mint
+LETTERS=(
+   ('PO','Poor'),
+   ('FR','Fair'),
+   ('AG','Almost Good'),
+   ('G','Good'),
+   ('VG','Very Good'),
+   ('F','Fine'),
+   ('VF','Very Fine'),
+   ('XF','Extra Fine'),
+   ('AU','Almost Good'),
+   ('MS','Mint-State'),
+   ('PF','Proof'),
+   ('SP','Specimen'),
+)
+NUMBERS=(
+   ('1','1'),
+   ('2','2'),
+   ('3','3'),
+   ('4','4-6'),
+   ('8','8-10'),
+   ('12','12-15'),
+   ('20','20-25'),
+   ('30','30-35'),
+   ('40','40-45'),
+   ('50','50-58'),
+   ('60','60-70'),
+)
+COMPANIES=(
+   ('NGC','NGC'),
+   ('PCGS','PCGS'),
+   ('ANACS','ANACS'),
+   ('Other','Other'),
+)
 class Coin(models.Model):
    name = models.CharField(max_length=100)
    origin = models.CharField(max_length=100)
@@ -12,3 +45,23 @@ class Coin(models.Model):
       return self.name
    def get_absolute_url(self):
       return reverse('detail',kwargs={'coin_id':self.id})
+
+class Grading(models.Model):
+   letter = models.CharField(
+      max_length=1,
+      choices=LETTERS,
+      default=LETTERS[0][0]
+      )
+   number = models.CharField(
+      max_length=1,
+      choices=NUMBERS,
+      default=NUMBERS[0][0]
+      )
+   company = models.CharField(
+      max_length=1,
+      choices=COMPANIES,
+      default=COMPANIES[0][0]
+      )
+   coin = models.ForeignKey(Coin,on_delete=models.CASCADE)
+   def __str__(self):
+      return f"Graded {self.get_grade_display()}"
