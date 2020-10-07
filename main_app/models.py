31751510e3a6b27819d84fe67e4dbe1560bafbe2
Edwin Huitz <EdwinHuitz@users.noreply.github.com>
2020-10-07 12:19:34 -0400

    adding update and delete functions

--- main_app/models.py
+++ main_app/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.urls import reverse
 # Create your models here.
 #name, origin, year, denom, mint
 class Coin(models.Model):
@@ -10,3 +10,5 @@ class Coin(models.Model):
    mint = models.CharField(max_length=100)
    def __str__(self):
       return self.name
+   def get_absolute_url(self):
+      return reverse('detail',kwargs={'coin_id':self.id})
--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from django.views.generic.edit import CreateView,UpdateView,DeleteView
 # Create your views here.
 from django.http import HttpResponse
 from .models import Coin
@@ -17,3 +17,15 @@ def coins_index(request):
 def coins_detail(request, coin_id):
   coin = Coin.objects.get(id=coin_id)
   return render(request, 'coins/detail.html', { 'coin': coin })
+class CoinCreate(CreateView):
+   model = Coin
+   fields = '__all__'
+   success_url = '/coins/'
+class CoinUpdate(UpdateView):
+  model = Coin
+  # Let's disallow the renaming of a cat by excluding the name field!
+  fields = ['origin', 'year', 'denom', 'mint']
+
+class CoinDelete(DeleteView):
+  model = Coin
+  success_url = '/coins/'
